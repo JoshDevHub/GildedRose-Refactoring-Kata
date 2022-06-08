3a8a0db17bbe71da96ee50e726abb71a3985e814
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-07 21:09:12 -0600

    refactor: introduce polymorphic `ItemUpdater`

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,45 +8,8 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      case item.name
-      when 'Aged Brie'
-        update_quality_for_brie(item)
-      when 'Sulfuras, Hand of Ragnaros'
-        update_quality_for_sulfras(item)
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        update_quality_for_backstage(item)
-      else
-        update_quality_for_default(item)
-      end
+      ItemUpdater.for(item).update
     end
-  end
-
-  private
-
-  def update_quality_for_brie(item)
-    item.sell_in -= 1
-    item.quality += 1 unless item.quality >= 50
-  end
-
-  def update_quality_for_sulfras(item); end
-
-  def update_quality_for_backstage(item)
-    item.sell_in -= 1
-    if item.sell_in <= 0
-      item.quality = 0
-      return
-    end
-    item.quality += 1
-    item.quality += 1 if item.sell_in <= 10
-    item.quality += 1 if item.sell_in <= 3
-  end
-
-  def update_quality_for_default(item)
-    item.sell_in -= 1
-    return if item.quality <= 0
-
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in <= 0
   end
 end
 
@@ -62,5 +25,72 @@ class Item
 
   def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+# item updater class
+class ItemUpdater
+  def self.for(item)
+    [BrieUpdater, BackstageUpdater, ItemUpdater, DefaultUpdater]
+      .find { |updater| updater.handles?(item.name) }
+      .new(item)
+  end
+
+  def self.handles?(name)
+    name == 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update; end
+
+  private
+
+  attr_reader :item
+end
+
+# update brie items
+class BrieUpdater < ItemUpdater
+  def self.handles?(name)
+    name == 'Aged Brie'
+  end
+
+  def update
+    item.sell_in -= 1
+    item.quality += 1 unless item.quality >= 50
+  end
+end
+
+# update backstage items
+class BackstageUpdater < ItemUpdater
+  def self.handles?(name)
+    name == 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  def update
+    item.sell_in -= 1
+    sell_in = item.sell_in
+    if sell_in <= 0
+      item.quality = 0
+      return
+    end
+    item.quality += 1
+    item.quality += 1 if sell_in <= 10
+    item.quality += 1 if sell_in <= 3
+  end
+end
+
+# update default item
+class DefaultUpdater < ItemUpdater
+  def self.handles?(_name) = true
+
+  def update
+    item.sell_in -= 1
+    return if item.quality <= 0
+
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in <= 0
   end
 end
